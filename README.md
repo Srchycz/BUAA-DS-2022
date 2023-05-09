eba81035063409d5df6cb56702ffa12779a7d693
Srchycz <2982296464@qq.com>
2023-05-09 11:12:18 +0800

    Add the corresponing question.pdf for each week.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # BUAA-DS-2022
+
+![license: MIT](https://img.shields.io/badge/license-MIT-green)
+
+Author: Srchycz
+
+## 仓库简介
+
+这是2022年春Srch的北航数据结构与程序设计课程的周作业代码及大作业代码
+
+完成度：通过所有周作业必做题目
+
+W1-W7即7次的周作业，对应文件夹内的Question.pdf文件包含了相应题面
+
+BW是大作业，经历了较多迭代，比较杂乱，同样有Question.pdf记录题面
+
+FinalTEST是期末考试文件夹，没有对应题面
+
+
+**PS：DS课程有严格的查重机制，本仓库可供往届题面及思路上的参考，切勿直接抄袭**
--- stu-bigwork/stu.c
+++ stu-bigwork/stu.c
@@ -1,203 +1,512 @@
-#define _CRT_SECURE_NO_WARNINGS
-#pragma GCC optimize ("O3")
-#pragma once
-#pragma pack (16)
+// #define _CRT_SECURE_NO_WARNINGS
+
 #include<stdio.h>
+#include<math.h>
 #include<stdlib.h>
+#include<ctype.h>
 #include<string.h>
 #include<time.h>
-#define SIZE 1000005
-#define Sens 120000
-struct sentence {
-	int sen[2];
-	int frequency;
-	int sentenceorder;
-}Sen[Sens];
-int i, j, m, l = 0, ll, n, flag = 0, temp, N, M;
-int SenNum = 0, senbegin[SIZE], VacNum = 0, VacFre[SIZE * 5], * VacFrePtr;
-char s[46600], tmp[200], buff[2000], ch; char st[Sens][45], hash[10001][130], na[Sens][40], id[Sens][40], sana[40];//s在这里定义
-int Trie[1000010][26];
-int Freqc[1000010] = { 0 };
-int pos = 1, p = 0, ar[Sens][130], sa[130], ct1 = 0;//ct1是停用词数量
-void getSW()//暂且不需要看
+#define EPS 1e-9
+#define PI 3.14159265358979323846
+#define maximum(x, y) ((x) > (y) ? (x) : (y))
+#define minimum(x, y) ((x) < (y) ? (x) : (y))
+typedef long long LL;
+typedef unsigned long long ULL;
+int gcd(int x, int y)
 {
-	freopen("stopwords.txt", "r", stdin);
-
-	while (scanf("%s", st[ct1++]) != EOF);
-
-	fclose(stdin);
+	return (y == 0) ? (x) : (gcd(y, x % y));
 }
-void clearSW()//暂且不需要看
+//以上是缺省源
+
+//===========================macro==========================
+
+#define WORD_LEN_MAX 250					//单词最长长度
+#define HASH_TABLE_SIZE 100003				//hash表的长度
+#define NMAX 10000							//N的最大值
+#define MMAX 128							//M的最大值
+#define FINGERPRINT_NUM	4		 			//fingerprint中unsigned int的个数
+
+
+//===========================struct==========================
+
+//字典树结构
+typedef struct TRIE
 {
-	for (int k = 0; k < ct1; k++)
-	{
-		p = 0;
-		for (int i = 0; st[k][i]; i++) {
-			temp = st[k][i] - '0';
-			if (!Trie[p][temp])
-				Trie[p][temp] = ++pos;//可以再优化
-			p = Trie[p][temp];
-		}
-		Freqc[p] = 0;
-	}
+	int flag;
+	struct TRIE *son[26];
+} trie;
+trie *trie_root = NULL;
 
-}
-
-int cmp(const void* a, const void* b)//暂且不需要看
+//hash表结构
+typedef struct HASHNODE
 {
-	return *(int*)b - *(int*)a;
-}
-int main()
+	char word[WORD_LEN_MAX];
+	int count;
+	int sub;					//sub表示在特征向量中的下标，默认-1
+	struct HASHNODE *next;
+} hashnode;
+hashnode *hash_table[HASH_TABLE_SIZE] = { 0 };
+hashnode *feature_table[NMAX + 1];
+
+//网页结构
+typedef struct WEB
 {
-	auto t1 = clock();
-	N = 10000, M = 16;
-	FILE* fp = fopen("sample.txt", "r");//问题主要是该读入的东西读入完后又读了些新的
-	ll=(int)fread(s, sizeof(char), SIZE * 7, fp);//这里读入sample
-	fclose(fp);//断点可以设置在这里，剩下的程序不用看了，原文本中sample以“reply\n\nmathew"结尾，程序已经读入了（在字符数组角标46498处左右读完，数组我开得比较贴合这个数字，调试时滑倒底再倒回去一点就行了）
-	FILE *out = fopen("temp.txt","w");
-    int i=0;
-    while(i<ll){
-        fputc(s[i++],out);
-    }
-    fclose(out);
-    /*
-	{
-	M = atoi(argv[1]);
-	N = atoi(argv[2]);
-	}
-	*/
-	// getSW();
+	char ID[WORD_LEN_MAX];
+	unsigned int fingerprint[FINGERPRINT_NUM];
+	struct WEB *next;
+} web;
+web *first_article = NULL, * first_sample = NULL;
 
-	// FILE* gp = fopen("hashvalue.txt", "r");
-	// for (int j = 0; j < 10000; j++)
-	// {
-	// 	fgets(hash[j], 129, gp);//貌似可以优化
-	// 	fgetc(gp);
-	// }
-	// for (int i = 0; i < N; i++)
-	// 	for (int j = 0; j < M; j++)
-	// 	{
-	// 		hash[i][j] -= '0';
-	// 	}
-	// fclose(gp); 
-	// int nu = 0;
-	// while (1)
-	// {
-	// 	if ((s[i] == ' ' || s[i] == '\n'))
-	// 	{
-	// 		if (flag)
-	// 		{
-	// 			i++;
-	// 			continue;
+//答案缓存结构
+typedef struct ANS_BUFF
+{
+	char *ID;
+	struct ANS_BUFF *next;
+} ans_buff;
+ans_buff *ans_first[4] = { 0 }, * ans_last[4] = { 0 };
 
-	// 		}
-	// 		else
-	// 			break;
-	// 	}
 
-	// 	flag = 0;
-	// 	id[0][nu++] = s[i];
-	// 	i++;
-	// }
-    
-	// while (i<=ll)
-	// {
-		// if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z'))
-		// {
-		// 	temp = (s[i] <= 'Z') ? s[i] - 'A' : s[i] - 'a';
-		// 	if (!Trie[p][temp])
-		// 		Trie[p][temp] = ++pos;
-		// 	p = Trie[p][temp];
-		// }
-		// else
-		// {
-		// 	Freqc[p]++;
-		// 	VacFre[++VacNum] = p;
-		// 	p = 0;
-		// }
-		// if (s[i] == '\f')
-		// {
+//===========================global==========================
 
-		// 	i++;
-		// 	clearSW();//结算
-		// 	qsort(Freqc, 1000010, sizeof(Freqc[0]), cmp);
-		// 	for (int i = 0; i < N; i++)
-		// 	{
-		// 		for (int cl = 0; cl < M; cl++)
-		// 		{
-		// 			if (hash[i][cl])
-		// 			{
-		// 				ar[j][cl] += Freqc[i];
-		// 			}
-		// 			else
-		// 			{
-		// 				ar[j][cl] -= Freqc[i];
-		// 			}
-		// 		}
-		// 	}
-		// 	for (int cl = 0; cl < M; cl++)
-		// 	{
-		// 		if (ar[j][cl] > 0)
-		// 			ar[j][cl] = 1;
-		// 		else
-		// 			ar[j][cl] = 0;
-		// 	}
-		// 	//			memset(Trie, 0, sizeof(Trie));
-		// 	memset(Freqc, 0, sizeof(Freqc));//为下一次做准备
-		// 	j++; if (j == 10)
-		// 	{
-		// 		for (int p = i - 10; p < i + 10; p++)
-		// 		{
-		// 			putchar(s[p]);
-		// 		}
-		// 	}
-			
-		// 	int flag = 1, nu = 0;
-		// 	while (s[i])//可以试试修改
-		// 	{
+int M, N;
+FILE *in_stopwords, * in_hashvalue, * in_article, * in_sample, * out_result;
+char *hashvalue_table;
+int word_count;					//统计hash表中一共有多少单词
 
-		// 		if ((s[i] == ' ' || s[i] == '\n'))
-		// 		{
-		// 			if (flag)
-		// 			{
-		// 				i++;
-		// 				continue;
 
-		// 			}
-		// 			else
-		// 				break;
-		// 		}
+//===========================state==========================
 
-		// 		flag = 0;
-		// 		id[j][nu++] = s[i];
-		// 		i++;
+void stopword_read();
+void hash_table_create();
+void feature_creat();
+web *fingerprint_creat(FILE *in);
+void ans_print();
+void hashvalue_read();
 
-		// 	}
 
-		// }
+int main(int argc, char **argv)
+{
+	N = atoi(argv[1]);
+	M = atoi(argv[2]);
 
-		
-		// i++;
+	in_stopwords = fopen("stopwords.txt", "r");
+	in_hashvalue = fopen("hashvalue.txt", "r");
+	in_article = fopen("article.txt", "r");
+	in_sample = fopen("sample.txt", "r");
+	out_result = fopen("result.txt", "w");
 
-	// }
-	// auto t2 = clock();
-	// printf("%d\n", (int)(t2 - t1));
-	/*
-	for (i = 0; i < 5; i++)
-	{
-		printf("%d ", Sen[i].frequency);
-		int l = Sen[i].sen[1];
-		j = Sen[i].sen[0];
-		while (s[j] == ' ' && j <= l) j++;
-		for (; j <= l; j++)
-		{
-			putchar(s[j]);
-		}
-		puts("");
-	}
-	freopen("results.txt", "w", stdout);
-	*/
-	
+	stopword_read();
+	hashvalue_read();
+	hash_table_create();
+	feature_creat();
+
+	first_article = fingerprint_creat(in_article);
+	first_sample = fingerprint_creat(in_sample);
+	ans_print();
+
+	fclose(in_stopwords);
+	fclose(in_hashvalue);
+	fclose(in_article);
+	fclose(in_sample);
+	fclose(out_result);
 
 	return 0;
+}
+
+//===========================proto==========================
+
+trie *trie_add(trie *root, char *p)
+{
+	if(root == NULL)
+	{
+		root = (trie *)calloc(1, sizeof(trie));
+	}
+	if(*p == '\0')
+	{
+		root->flag = 1;
+		return root;
+	}
+	root->son[*p - 'a'] = trie_add(root->son[*p - 'a'], p + 1);
+	return root;
+}
+
+//读取stopword并且存于字典树trie_root中
+void stopword_read()
+{
+	char stopword[WORD_LEN_MAX] = { 0 };
+	while(fgets(stopword, WORD_LEN_MAX, in_stopwords) != NULL)
+	{
+		stopword[strlen(stopword) - 1] = '\0';
+		trie_root = trie_add(trie_root, stopword);
+	}
+}
+
+//读取hash的前N行前M列
+void hashvalue_read()
+{
+	char laji[MMAX];
+	hashvalue_table = calloc(N * M + 1, sizeof(char));
+	for(int i = 0; i < N; i++)
+	{
+		fgets(hashvalue_table + i * M, M + 1, in_hashvalue);
+		fgets(laji, MMAX, in_hashvalue);
+	}
+}
+
+//从指定文件中读取一个单词
+//单词存放于word中
+//返回值为1说明未到网页尾
+int fgetword(FILE *in, char *word)
+{
+	char *p = word;
+	char ch = 0;
+	while(!isalpha(ch = fgetc(in)))
+	{
+		if(ch == '\f')
+		{
+			return 0;
+		}
+		if(ch == EOF)
+		{
+			return -1;
+		}
+	}
+	ungetc(ch, in);
+	while(1)
+	{
+		ch = fgetc(in);
+		if(islower(ch))
+		{
+			*p = ch;
+			p++;
+		}
+		else if(isupper(ch))
+		{
+			*p = tolower(ch);
+			p++;
+		}
+		else
+		{
+			*p = '\0';
+			if(ch == '\f')
+			{
+				return 0;
+			}
+			else if(ch == EOF)
+			{
+				return EOF;
+			}
+			else
+			{
+				return 1;
+			}
+		}
+	}
+}
+
+//判断word是否在停用词表中
+int isstopword(char *word)
+{
+	int sub = 0;
+	trie *root = trie_root;
+	while(root != NULL)
+	{
+		if(word[sub] == '\0')
+		{
+			return root->flag;
+		}
+		root = root->son[word[sub] - 'a'];
+		sub++;
+	}
+	return 0;
+}
+
+//hash函数
+unsigned int hash(char *key)
+{
+	unsigned int hash_val = 0;
+	while(*key != '\0')
+	{
+		hash_val = hash_val * 31 + *key - 'a' + 1;
+		key++;
+	}
+	return hash_val % HASH_TABLE_SIZE;
+}
+
+//将word添加到hash表中
+void hash_table_add(char *word)
+{
+	unsigned int hash_val = hash(word);
+	hashnode *pnode = hash_table[hash_val], * prepnode = NULL;
+	while(pnode != NULL)
+	{
+		if(strcmp(pnode->word, word) == 0)
+		{
+			pnode->count++;
+			return;
+		}
+		prepnode = pnode;
+		pnode = pnode->next;
+	}
+	hashnode *q = (hashnode *)malloc(sizeof(hashnode));
+	strcpy(q->word, word);
+	q->count = 1;
+	q->sub = -1;
+	q->next = NULL;
+	if(prepnode == NULL)
+	{
+		hash_table[hash_val] = q;
+	}
+	else
+	{
+		prepnode->next = q;
+	}
+	word_count++;
+}
+
+//建造hash_table
+//in_article文件指针会返回文件首
+void hash_table_create()
+{
+	char ch = 0;
+	char ID[WORD_LEN_MAX] = { 0 };
+
+	while((ch = fgetc(in_article)) != EOF)
+	{
+		ungetc(ch, in_article);
+
+		fscanf(in_article, "%s", ID);
+
+		int flag = 0;
+		while(1)
+		{
+			char word[WORD_LEN_MAX] = { 0 };
+			flag = fgetword(in_article, word);
+			if(flag != 1)
+			{
+				break;
+			}
+			if(strlen(word) != 0)
+			{
+				if(!isstopword(word))
+				{
+					hash_table_add(word);
+				}
+			}
+		}
+		if(flag == -1)
+		{
+			break;
+		}
+	}
+
+	fseek(in_article, 0, SEEK_SET);
+}
+
+//特征向量重排
+void feature_resort()
+{
+	int sub = N;
+	while(sub != 0 && (feature_table[sub - 1] == NULL || feature_table[sub - 1]->count < feature_table[sub]->count || (feature_table[sub - 1]->count == feature_table[sub]->count && strcmp(feature_table[sub]->word, feature_table[sub - 1]->word) < 0)))
+	{
+		hashnode *temp = feature_table[sub];
+		feature_table[sub] = feature_table[sub - 1];
+		feature_table[sub - 1] = temp;
+		sub--;
+	}
+}
+
+//生成网页特征向量
+void feature_creat()
+{
+	for(int i = 0; i < HASH_TABLE_SIZE; i++)
+	{
+		hashnode *q = hash_table[i];
+		while(q != NULL)
+		{
+			feature_table[N] = q;
+			feature_resort();
+			q = q->next;
+		}
+	}
+
+	for(int i = 0; i < N; i++)
+	{
+		feature_table[i]->sub = i;
+	}
+}
+
+//为一串网页生成指纹
+web *fingerprint_creat(FILE *in)
+{
+	web *first = NULL, * p = NULL, * q = NULL;
+	char ch = 0;
+
+	while((ch = fgetc(in)) != EOF)
+	{
+		ungetc(ch, in);
+
+		q = (web *)calloc(1, sizeof(web));
+		fscanf(in, "%s", q->ID);
+		if(first == NULL)
+		{
+			first = p = q;
+		}
+		else
+		{
+			p->next = q;
+			p = q;
+		}
+
+		int count[NMAX] = { 0 };
+		int flag = 0;
+		while(1)
+		{
+			char word[WORD_LEN_MAX] = { 0 };
+			flag = fgetword(in, word);
+			if(flag != 1)
+			{
+				break;
+			}
+			if(strlen(word) != 0)
+			{
+				hashnode *pnode = hash_table[hash(word)];
+				while(pnode != NULL)
+				{
+					if(strcmp(pnode->word, word) == 0)
+					{
+						int sub = pnode->sub;
+						if(sub != -1)
+						{
+							count[sub]++;
+							break;
+						}
+					}
+					pnode = pnode->next;
+				}
+
+			}
+		}
+
+		int sum[MMAX] = { 0 };
+		for(int i = 0; i < N; i++)
+		{
+			for(int j = 0; j < M; j++)
+			{
+				(hashvalue_table[i * M + j] - '0') ? (sum[j] += count[i]) : (sum[j] -= count[i]);
+			}
+		}
+		for(int i = 0; i < M; i++)
+		{
+			q->fingerprint[i / 32] = q->fingerprint[i / 32] << 1;
+			if(sum[i] > 0)
+			{
+				q->fingerprint[i / 32] ++;
+			}
+		}
+
+		if(flag == -1)
+		{
+			break;
+		}
+	}
+
+	return first;
+}
+
+//计算距离
+int distance_cal(unsigned int x, unsigned int y)
+{
+	int distance = 0;
+	unsigned int z = x ^ y;
+	while(z != 0)
+	{
+		z &= (z - 1);
+		distance += 1;
+	}
+	return distance;
+}
+
+//缓存结果
+void ans_buff_add(char *ID, int distance)
+{
+	ans_buff *q = (ans_buff *)malloc(sizeof(ans_buff));
+	q->ID = ID;
+	q->next = NULL;
+	if(ans_first[distance] == NULL)
+	{
+		ans_first[distance] = ans_last[distance] = q;
+	}
+	else
+	{
+		ans_last[distance]->next = q;
+		ans_last[distance] = q;
+	}
+}
+
+//输出一个sample网页的结果
+// flag为1表示需要输出到stdout
+//同时释放缓冲区空间
+void ans_web_print(char sample_ID[WORD_LEN_MAX], int flag)
+{
+	fprintf(out_result, "%s\n", sample_ID);
+	if(flag)
+	{
+		fprintf(stdout, "%s\n", sample_ID);
+	}
+	for(int i = 0; i <= 3; i++)
+	{
+		ans_buff *p = ans_first[i];
+		if(p != NULL)
+		{
+			fprintf(out_result, "%d:", i);
+			if(flag)
+			{
+				fprintf(stdout, "%d:", i);
+			}
+			while(p != NULL)
+			{
+				fprintf(out_result, "%s ", p->ID);
+				if(flag)
+				{
+					fprintf(stdout, "%s ", p->ID);
+				}
+				ans_buff *prep = p;
+				p = p->next;
+				free(prep);
+			}
+			fputc('\n', out_result);
+			if(flag)
+			{
+				fputc('\n', stdout);
+			}
+			ans_first[i] = ans_last[i] = NULL;
+		}
+	}
+}
+
+//输出结果
+void ans_print()
+{
+	web *psample = first_sample;
+	while(psample != NULL)
+	{
+		web *particle = first_article;
+		while(particle != NULL)
+		{
+			int distance = 0;
+			for(int i = 0; i < FINGERPRINT_NUM; i++)
+			{
+				distance += distance_cal(particle->fingerprint[i], psample->fingerprint[i]);
+			}
+			if(distance <= 3)
+			{
+				ans_buff_add(particle->ID, distance);
+			}
+			particle = particle->next;
+		}
+
+		ans_web_print(psample->ID, psample == first_sample);
+		psample = psample->next;
+	}
 }
